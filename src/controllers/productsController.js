219b2440784eb12e85ac70607d066aed4464a526
tomikaufmann <tomas.kaufmann@osh-pro.com>
2021-09-05 21:30:44 -0300

    wip: POST /products → req.body no funca

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 const productList = JSON.parse(
     fs.readFileSync(path.join(__dirname, "../data/products.json"))
 );
+
+function writeJSON(data) {
+    fs.writeFileSync(path.join(__dirname, "../data/products.json", JSON.parse(data)))
+}
+
 const controlador = {
     home: (req, res) => {
         res.render(path.join(__dirname, "../views/index.ejs"), { productList })
@@ -19,9 +24,29 @@ const controlador = {
         );
         res.render(path.join(__dirname, "../views/productDetail.ejs"), { product })
     },
-    create: (req, res) => { 
+    create: (req, res) => {
 
-        res.render(path.join(__dirname,"../views/crearProducto.ejs"))
+        res.render(path.join(__dirname, "../views/crearProducto.ejs"))
+    },
+    store: (req, res) => {
+        console.log(req.body)
+        // const newProduct = {
+        //     id: productList+1 ,
+        //     nombre: req.body.nombre ,
+        //     desc: req.body.desc ,
+        //     imagen: req.body.imagen ,
+        //     categoria: req.body.categoria ,
+        //     precio: req.body.precio
+        // }
+
+        // let addProduct = [...productList, newProduct]
+
+        // writeJSON(addProduct);
+    
+        res.send("asd")
+
+        // res.redirect("/products")
+
     }
 }
 
