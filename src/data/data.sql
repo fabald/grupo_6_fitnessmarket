8f0c4b41387785d1588b4de798605a4de0d0a34b
TomasKaufmann <tomas.kaufmann@osh-pro.com>
2021-10-28 10:50:39 -0300

    wip: CRUD con sequelize

--- src/data/data.sql
+++ src/data/data.sql
@@ -5,42 +5,25 @@ INSERT INTO `users` VALUES (1,'Tomas','Kaufmann','tomaskaufmann13@gmail.com','$2
 (4,'Brian','Catrini','briancatrini87@gmail.com','','userPicture-image2.jpeg');
 UNLOCK TABLES;
 
+LOCK TABLES `categories` WRITE;
+INSERT INTO `categories` VALUES (1,'Indumentaria'),(2,'Suplementos'),(3,'Equipamiento'), (4, 'Musculacion');
+UNLOCK TABLES;
 
 LOCK TABLES `products` WRITE;
-INSERT INTO `products` VALUES (1,1,'Banco Plano','Banco plano con imagen',890.50,'Gratis','product-1632004612392.jpg'),
-(2,4,'Mancuerna Hexagonal','Mancuerna Hexagonal de 25kg',650,'Gratis','product-1632005064113.jpg'),
-(3,2,'Soga Para Saltar','Soga Para Saltar Naranja Entrenamiento Gym Fitness',399,'Gratis','product-1632693718371.jpg'),
-(4,3,'Life Pro Whey','Life Pro Whey Protein 2kg New',5822.73,'Gratis','product-image5.jpeg'),
-(5,4,'Remera Training FILA','Remera Training Negra FILA Basic Sport Hombre',2592,'Gratis','product-image6.jpeg');
+INSERT INTO `products` VALUES (1,'Banco Plano','Banco plano con imagen',890.50,'Techno Gym','product-1632004612392.jpg',1,4),
+(2,'Mancuerna Hexagonal','Mancuerna Hexagonal de 25kg',650,'Techno Gym','product-1632005064113.jpg',1,4),
+(3,'Soga Para Saltar','Soga Para Saltar Naranja Entrenamiento Gym Fitness',399,'Techno Gym','product-1632693718371.jpg',1,2),
+(4,'Life Pro Whey','Life Pro Whey Protein 2kg New',5822.73,'Techno Gym','product-image5.jpg',1,2),
+(5,'Remera Training FILA','Remera Training Negra FILA Basic Sport Hombre',2592,'Techno Gym','product-image6.jpg',1,2);
 UNLOCK TABLES;
 
 
-LOCK TABLES `categories` WRITE;
-INSERT INTO `categories` VALUES (1,'Indumentaria'),(2,'Suplementos'),(3,'Equipamiento');
+LOCK TABLES `carts` WRITE;
+INSERT INTO `carts` VALUES (1,1,'2021-02-25',890.50),(2,2,'2021-03-10',650.50),
+(3,3,'2021-05-02',399.00),(4,1,'21-10-04',5822.73),(5,1,'2021-10-18',2592.00);
 UNLOCK TABLES;
 
 
-LOCK TABLES `products_categories` WRITE;
-INSERT INTO `products_categories` VALUES (1,1,3),(2,2,3),(3,3,3),(4,4,2),(5,5,1);
-UNLOCK TABLES;
-
-
-LOCK TABLES `brands` WRITE;
-INSERT INTO `brands` VALUES (1,'NutriMarket'),(2,'Life Pro'),(3,'Mir Fitness'),(4,'Assault Fitness'),(5,'FILA'),(6,'Under Armour');
-UNLOCK TABLES;
-
-
-LOCK TABLES `products_brands` WRITE;
-INSERT INTO `products_brands` VALUES (1,3,3),(2,4,1),(3,4,2),(4,2,4),(5,3,5);
-UNLOCK TABLES;
-
-
-LOCK TABLES `cart` WRITE;
-INSERT INTO `cart` VALUES (1,'Tomas Kaufmann','2021-02-25',890.50),(2,'Brian Catrini','2021-03-10',650),
-(3,'Nicolas Blasutig','2021-05-02',399),(4,'Fabian Alderete','21-10-04',5822.73),(5,'Brian Catrini','2021-10-18',2592);
-UNLOCK TABLES;
-
-
-LOCK TABLES `product_cart` WRITE;
-INSERT INTO `product_cart` VALUES (1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5);
+LOCK TABLES `product_carts` WRITE;
+INSERT INTO `product_carts` VALUES (1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5);
 UNLOCK TABLES;
--- src/data/structure.sql
+++ src/data/structure.sql
@@ -4,84 +4,55 @@ USE fitness_db;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
-  `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_img` varchar(250) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
- );
-
-
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE `products` (
-`product_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`user_id` int(10) UNSIGNED NOT NULL,
-`product_name` varchar(100) NOT NULL,
-`description` text DEFAULT NULL,
-`price` DECIMAL(4, 1) NOT NULL,
-`shipment_type` varchar(100) DEFAULT NULL,
-`product_img` varchar(200) DEFAULT NULL,
-PRIMARY KEY (`product_id`),
-FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`email`)
 );
-
 
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE `categories` (
-`category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`category_name` varchar(100) NOT NULL,
-PRIMARY KEY (`category_id`)
+`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`name` varchar(100) NOT NULL,
+PRIMARY KEY (`id`)
 );
 
-
-DROP TABLE IF EXISTS `products_categories`;
-CREATE TABLE `products_categories` (
-`products_category_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`product_id` int(10) UNSIGNED NOT NULL,
-`category_id` int(10) UNSIGNED NOT NULL,
-PRIMARY KEY (`products_category_id`),
-FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`),
-FOREIGN KEY (`category_id`) REFERENCES `categories`(`category_id`)
-);
-
-
-DROP TABLE IF EXISTS `brands`;
-CREATE TABLE `brands` (
-`brand_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+`id` int(10) UNSIGNED NOT NULL auto_increment,
+`product_name` varchar(100) NOT NULL,
+`description` text DEFAULT NULL,
+`price` DECIMAL(6, 2) NOT NULL,
 `brand_name` varchar(100) NOT NULL,
-PRIMARY KEY (`brand_id`)
+`product_img` varchar(200) DEFAULT NULL,
+`user_id` int(10) UNSIGNED NOT NULL,
+`category_id` int(10) UNSIGNED NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`user_id`) REFERENCES `users`(`id`),
+FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`)
 );
-
-
-DROP TABLE IF EXISTS `products_brands`;
-CREATE TABLE `products_brands` (
-`products_brand_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-`brand_id` int(10) UNSIGNED NOT NULL,
-`product_id` int(10) UNSIGNED NOT NULL,
-PRIMARY KEY (`products_brand_id`),
-FOREIGN KEY (`brand_id`) REFERENCES `brands`(`brand_id`),
-FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`)
-);
-
 
 DROP TABLE IF EXISTS `carts`;
 CREATE TABLE `carts` (
-`cart_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-`buyer_name` varchar(100) NOT NULL,
+`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`user_id` int(10) UNSIGNED NOT NULL,
 `buy_date` date DEFAULT NULL,
-`total` DECIMAL(4, 1) NOT NULL,
-PRIMARY KEY (`cart_id`)
+`total` DECIMAL(6, 2) NOT NULL,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`user_id`) REFERENCES `users`(`id`)
 );
 
 
 DROP TABLE IF EXISTS `product_carts`;
 CREATE TABLE `product_carts` (
-`product_cart_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 `cart_id` int(10) UNSIGNED NOT NULL,
 `product_id` int(10) UNSIGNED NOT NULL,
-PRIMARY KEY (`product_cart_id`),
-FOREIGN KEY (`cart_id`) REFERENCES `carts`(`cart_id`),
-FOREIGN KEY (`product_id`) REFERENCES `products`(`product_id`)
+PRIMARY KEY (`id`),
+FOREIGN KEY (`cart_id`) REFERENCES `carts`(`id`),
+FOREIGN KEY (`product_id`) REFERENCES `products`(`id`)
 );
