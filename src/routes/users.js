843a84551a7b011736d1a2890585be055bc10aa2
Nico Blasutig <nicolasblasutig@gmail.com>
2021-11-24 12:40:43 -0300

    validaciones register empezadas

--- src/routes/users.js
+++ src/routes/users.js
@@ -18,11 +18,26 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+const {check} = require("express-validator")
+
+const validateRegister = [
+    check("nombre")
+        .notEmpty().withMessage("Este campo es Obligatorio.").bail()
+        .isLength({min:2}).withMessage("El nombre debe tener como minimo 2 caracteres."),
+    check("email")
+        .notEmpty().withMessage("Este campo es Obligatorio.").bail()
+        .isEmail().withMessage("El mail debe ser valido."),
+    check("password")
+        .notEmpty().withMessage("Este campo es Obligatorio.").bail()
+        .isLength({min:8})
+    // falta validacion de imagen y de chquear mail no existente en base de datos.
+
+]
 
 
 router.get("/login", guestMiddleware, usersController.login);
 
-router.get("/register", guestMiddleware, usersController.register);
+router.get("/register", guestMiddleware, validateRegister,  usersController.register);
 
 router.post("/register/create", upload.single("imagenUsuario"), usersController.store);
 
