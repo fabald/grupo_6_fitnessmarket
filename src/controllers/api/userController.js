3863ff98ca058ba958b02e7b4f2fbc1ce574aa1d
Nico Blasutig <nicolasblasutig@gmail.com>
2021-11-29 23:52:11 -0300

    Devolviendo la info que queremos en las Api User

--- src/controllers/api/userController.js
+++ src/controllers/api/userController.js
@@ -1,15 +1,39 @@
+const fs = require('fs');
 const path = require("path");
 const db = require("../../database/models");
 const Op = db.Sequelize.Op;
 
+
+
 const userController = {
+
     list: (req, res) => {
         db.User.findAll()
         .then(usuarios =>{
+            function objetoChanger(objeto){
+                let objetoNuevo = {
+                    id: objeto.id,
+                    name: objeto.first_name + objeto.last_name,
+                    email: objeto.email,
+                    ProfilPic: objeto.user_img
+                }
+                return objetoNuevo
+            
+            }
+            
+            function dataForApi(arrayconDataVieja){
+                let arrayNuevo = []
+                for(let i = 0; i < arrayconDataVieja.length; i++ ){
+                    arrayNuevo.push(objetoChanger(arrayconDataVieja[i]))
+                }
+            
+                return arrayNuevo
+            }
+            
             return res.status(200).json({
                 url: "http://localhost:3050/api/users",
                 total: usuarios.length,
-                data: usuarios,
+                data: dataForApi(usuarios),
                 status: 200
             })
         })
